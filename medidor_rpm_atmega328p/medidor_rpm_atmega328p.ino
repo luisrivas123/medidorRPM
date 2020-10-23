5c6c8b3daa59f852e1557ae3e6db312d671c3720
luisrivas123 <luis.rivas@correounivalle.edu.co>
2020-10-23 18:50:20 -0500

    commit201023

--- medidor_rpm_atmega328p/medidor_rpm_atmega328p.ino
+++ medidor_rpm_atmega328p/medidor_rpm_atmega328p.ino
@@ -2,20 +2,19 @@
 // Conexion de la entrada de la interrupcion 0  por el PIN 2
 // Configurar el monitor serial a 57600 Baudios para visualizar los RPM
 // Tienda para comprar Materiales http://dinastiatecnologica.com
-#include <TinyWireM.h>
-#include <TinyLiquidCrystal.h>
+#include <LiquidCrystal_I2C.h>
 
-//LiquidCrystal_I2C lcd(0x27,16,2); // configuraciones degun el fabricante 0x3F , 0x20 , 0x38
-TinyLiquidCrystal lcd(0x27);
+LiquidCrystal_I2C lcd(0x27,16,2); // configuraciones degun el fabricante 0x3F , 0x20 , 0x38
+
 
 volatile int contador = 0;   // Variable entera que se almacena en la RAM del Micro
  
 void setup() {
   //Serial.begin(57600);
   attachInterrupt(1,interrupcion0,RISING);  // Interrupcion 1 (pin1) // LOW, CHANGE, RISING, FALLING
-  TinyWireM.begin();                    // initialize I2C lib
-  lcd.begin(16, 2);            // our display has 16 cols, 2 rows
-  lcd.setBacklight(HIGH);      // Turn on the backlight
+  lcd.init();
+  lcd.home();
+  lcd.backlight();
   lcd.print("Lector RPM");  // Print a message to the LCD.
   delay(3000);
 }  
