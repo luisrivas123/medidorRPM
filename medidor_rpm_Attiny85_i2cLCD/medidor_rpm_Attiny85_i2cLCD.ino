4e9c2cfb61c6273cfcda888462bb23a55af10cad
luisrivas123 <luis.rivas@correounivalle.edu.co>
2020-10-25 22:21:43 -0500

    raspberry21019

--- medidor_rpm_Attiny85_i2cLCD/medidor_rpm_Attiny85_i2cLCD.ino
+++ medidor_rpm_Attiny85_i2cLCD/medidor_rpm_Attiny85_i2cLCD.ino
@@ -1,8 +1,8 @@
-
 #include <TinyWireM.h>
 #include <USI_TWI_Master.h>
 #include <LCD.h>
 #include <LiquidCrystal_I2C.h>
+#include <PinChangeInterrupt.h>
  
 #define I2C_ADDR    0x27 // <<----- Add your address here.  Find it from I2C Scanner
 #define BACKLIGHT_PIN     3
@@ -13,14 +13,19 @@
 #define D5_pin  5
 #define D6_pin  6
 #define D7_pin  7 
- 
+
+const int Banda = 150;
+long Time = 0;
+const int pininterrupt = 1;
 LiquidCrystal_I2C  lcd(I2C_ADDR,En_pin,Rw_pin,Rs_pin,D4_pin,D5_pin,D6_pin,D7_pin);
 
 volatile int contador = 0;   // Variable entera que se almacena en la RAM del Micro
  
 void setup() {
 
-  attachInterrupt(1,interrupcion0,RISING);  // Interrupcion 1 (pin1) // LOW, CHANGE, RISING, FALLING
+  attachPinChangeInterrupt(digitalPinToPinChangeInterrupt (pininterrupt),interrupcion0,FALLING);  // Interrupcion 1 (pin1) // LOW, CHANGE, RISING, FALLING
+  pinMode(pininterrupt,INPUT);
+  //attachInterrupt(1,interrupcion0,RISING);
   TinyWireM.begin();                    // initialize I2C lib
   lcd.begin (16,2); // 
   lcd.setBacklightPin(BACKLIGHT_PIN,POSITIVE);
@@ -36,11 +41,19 @@ void loop() {
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(contador*60); // Como son dos interrupciones por vuelta (contador * (60/2))
-  lcd.println(" RPM     ");    //  El numero 2 depende del numero aspas de la helise del motor en prueba
+  lcd.print(" RPM          ");    //  El numero 2 depende del numero aspas de la helise del motor en prueba
+  lcd.setCursor(0,1);
+  lcd.print(contador); // Como son dos interrupciones por vuelta (contador * (60/2))
+  lcd.print(" RPS          ");    //  El numero 2 depende del numero aspas de la helise del motor en prueba
   contador = 0;
 }
  
 void interrupcion0()    // Funcion que se ejecuta durante cada interrupion
 {
   contador++;           // Se incrementa en uno el contador
+  //
+  /*if(millis() - Time > Banda){
+    contador++;
+    Time = millis();
+  }*/
 }
--- medidor_rpm_atmega328p/medidor_rpm_atmega328p.ino
+++ medidor_rpm_atmega328p/medidor_rpm_atmega328p.ino
@@ -3,15 +3,19 @@
 // Configurar el monitor serial a 57600 Baudios para visualizar los RPM
 // Tienda para comprar Materiales http://dinastiatecnologica.com
 #include <LiquidCrystal_I2C.h>
+//#include <PinChangeInterrupt.h>
 
 LiquidCrystal_I2C lcd(0x27,16,2); // configuraciones degun el fabricante 0x3F , 0x20 , 0x38
 
+const int pininterrupt = 2;
 
 volatile int contador = 0;   // Variable entera que se almacena en la RAM del Micro
  
 void setup() {
   Serial.begin(57600);
+  //pinMode(pininterrupt,INPUT);
   attachInterrupt(0,interrupcion0,RISING);  // Interrupcion 1 (pin1) // LOW, CHANGE, RISING, FALLING
+  //attachInterrupt(digitalPinToInterrupt (pininterrupt),interrupcion0,FALLING);  // Interrupcion 1 (pin1) // LOW, CHANGE, RISING, FALLING
   lcd.init();
   lcd.home();
   lcd.backlight();
@@ -21,13 +25,16 @@ void setup() {
 
  
 void loop() {
-  delay(999);               // retardo de casi 1 segundo
+  delay(990);               // retardo de casi 1 segundo
   lcd.clear();
   lcd.setCursor(0,0);
   lcd.print(contador*60); // Como son dos interrupciones por vuelta (contador * (60/2))
   lcd.print(" RPM            ");    //  El numero 2 depende del numero aspas de la helise del motor en prueba
-  Serial.print(contador*60); // Como son dos interrupciones por vuelta (contador * (60/2))
-  Serial.println(" RPM            "); 
+  lcd.setCursor(0,1);
+  lcd.print(contador); // Como son dos interrupciones por vuelta (contador * (60/2))
+  lcd.print(" RPS            ");    //  El numero 2 depende del numero aspas de la helise del motor en prueba
+  //Serial.print(contador*60); // Como son dos interrupciones por vuelta (contador * (60/2))
+  //Serial.println(" RPM            "); 
   contador = 0;
 }
  
